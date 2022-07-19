77cd2370c125de1f59819ce67d73e4ac6d28fbf5
Tohid abrian <87374919+tohid-ab@users.noreply.github.com>
2022-07-19 12:40:25 +0430

    Update sms_sender.py

--- api/sms_sender.py
+++ api/sms_sender.py
@@ -1,21 +1,5 @@
 from kavenegar import *
 
-
-# try:
-#   api = KavenegarAPI('your apikey here')
-#   params = {
-#       'receptor': '',
-#       'template': '',
-#       'token': '',
-#       'type': 'sms',  # sms vs call
-#   }
-#   response = api.verify_lookup(params)
-#   print(response)
-# except APIException as e:
-#   print(e)
-# except HTTPException as e:
-#   print(e)
-
 def kavenegar_token_send(token):
     print('OTP USER')
     print(f'code: {token.password}')
